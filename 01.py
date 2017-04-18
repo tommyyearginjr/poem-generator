b637edacd175fd8f30ea1e5dc1c5a8fc77ea82f0
Tommy H. Yeargin, Jr <tommyyearginjr@users.noreply.github.com>
2017-04-18 13:30:21 -0400

    Update 01.py

--- 01.py
+++ 01.py
@@ -13,3 +13,6 @@ for i in words:
 			uniqueWords.append(i)
 		
 print(uniqueWords)
+"""
+>> ['four', 'score', 'and', 'seven', 'years', 'ago', 'our', 'fathers', 'brought', 'forth', 'on', 'this', 'continent', 'a', 'new', 'nation', 'conceived', 'in', 'liberty', 'dedicated', 'to', 'the', 'proposition', 'that', 'all', 'men', 'are', 'created', 'equal', 'now', 'we', 'engaged', 'great', 'civil', 'war', 'testing', 'whether', 'or', 'any', 'so', 'can', 'long', 'endure', 'met', 'battle-field', 'of', 'have', 'come', 'dedicate', 'portion', 'field', 'as', 'final', 'resting', 'place', 'for', 'those', 'who', 'here', 'gave', 'their', 'lives', 'might', 'live', 'it', 'is', 'altogether', 'fitting', 'proper', 'should', 'do', 'but', 'larger', 'sense', 'not', 'consecrate', 'hallow', 'ground', 'brave', 'living', 'dead', 'struggled', 'consecrated', 'far', 'above', 'poor', 'power', 'add', 'detract', 'world', 'will', 'little', 'note', 'nor', 'remember', 'what', 'say', 'never', 'forget', 'they', 'did', 'us', 'rather', 'be', 'unfinished', 'work', 'which', 'fought', 'thus', 'nobly', 'advanced', 'task', 'remaining', 'before', 'from', 'these', 'honored', 'take', 'increased', 'devotion', 'cause', 'last', 'full', 'measure', 'highly', 'resolve', 'shall', 'died', 'vain', 'under', 'god', 'birth', 'freedom', 'government', 'people', 'by', 'perish', 'earth']
+"""
