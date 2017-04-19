0a4f591de7cea875c9a5a414fbe1c6d4e88bf200
Tommy H. Yeargin, Jr <tommy.yeargin@gmail.com>
2017-04-19 18:46:35 -0400

    change is good.

--- 01.py
+++ 01.py
@@ -17,15 +17,17 @@ for i in words:
 			uniqueWords.append(i)
 
 # The following is what actually generates the poem, using the unique words list
-# created above. The first range ('0,100') is the number of words to be used in
+# created above. The first range ('0,100') is total the number of words to be used in
 # the output. Change this to as long or as short as you wish. The second range
 # is what actually generates the number of words per line. Play around with this,
 # to get varying lengths that suit you. Have fun!
 
 poem_raw = list()
 
+wordsPerLine = 8
+
 for i in range(0,100):
 	poem_raw.append(random.choice(uniqueWords))
 
-for i in range(len(poem_raw)/5+1):
-    print " ".join(poem_raw[i*5:(i+1)*5])
+for i in range(len(poem_raw)/wordsPerLine+1):
+    print " ".join(poem_raw[i*wordsPerLine:(i+1)*wordsPerLine])
